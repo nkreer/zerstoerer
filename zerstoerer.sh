4b9ffc7ecff006cc9e42b93860d1697dbbbad643
Niklas Kreer <kreergit@posteo.de>
2019-06-07 19:39:51 +0200

    Automatically overwrite

--- zerstoerer.sh
+++ zerstoerer.sh
@@ -4,10 +4,10 @@
 espeak -v de -w "tmp.wav" "$1"
 
 # Replace the audio in the Amthor video
-ffmpeg -i segments/rezo.mp4 -i tmp.wav -shortest -vcodec copy -acodec aac -ac 2 -ar 48000 -b:a 256k custom.mp4
+ffmpeg -i segments/rezo.mp4 -i tmp.wav -shortest -vcodec copy -acodec aac -ac 2 -ar 48000 -b:a 256k custom.mp4 -y
 
 # Concatenate everything back together
-ffmpeg -f concat -i order.txt -vcodec copy output.mp4
+ffmpeg -f concat -i order.txt -vcodec copy output.mp4 -y
 
 # Play back
 mpv output.mp4
