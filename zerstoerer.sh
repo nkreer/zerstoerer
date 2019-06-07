888950e6cf8b91a6fd77cdf38eb37743bbe4617e
Niklas Kreer <kreergit@posteo.de>
2019-06-07 19:26:02 +0200

    Create your own Philipp Amthor memes

---
+++ zerstoerer.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Generate the voice
+espeak -v de -w "tmp.wav" "$1"
+
+# Replace the audio in the Amthor video
+ffmpeg -i segments/rezo.mp4 -i tmp.wav -shortest -vcodec copy -acodec aac -ac 2 -ar 48000 -b:a 256k custom.mp4
+
+# Concatenate everything back together
+ffmpeg -f concat -i order.txt -vcodec copy output.mp4
+
+# Play back
+mpv output.mp4
